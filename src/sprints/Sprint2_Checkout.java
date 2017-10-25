ad8afc3702d29cd03d96a58d5ce95f0ca0d1267c
chenglinwu <jasonwu.601@gmail.com>
2017-10-25 02:32:34 -0400

    Update

--- src/sprints/Sprint2_Checkout.java
+++ src/sprints/Sprint2_Checkout.java
@@ -32,32 +32,42 @@ public class Sprint2_Checkout {
         us36_list_recent_deaths(recent_deaths);
 
 			for (Family fam : familyList) {
-				String husband_id = null;
-				String husband_name = null;
-				Date husband_death = null;
-				Date husband_birth = null;
+                String husband_id = null;
+                String husband_name = null;
+                String husband_gender = null;
+                Date husband_death = null;
+                Date husband_birth = null;
 
-				String wife_id = null;
-				String wife_name = null;
-				Date wife_death = null;
-				Date wife_birth = null;
+                String wife_id = null;
+                String wife_name = null;
+                String wife_gender = null;
+                Date wife_death = null;
+                Date wife_birth = null;
+
+                List<String> children = fam.getChildren();
 
 				for (int i = 0; i < indivdualList.size(); ++i) {
 					if (fam.getHusbandID().equals(indivdualList.get(i).getID())) {
 						husband_id = indivdualList.get(i).getID();
 						husband_name = indivdualList.get(i).getName();
+						husband_gender = indivdualList.get(i).getGender();
 						husband_death = indivdualList.get(i).getDeath();
 						husband_birth = indivdualList.get(i).getBirthday();
 					}
 					if (fam.getWifeID().equals(indivdualList.get(i).getID())) {
 						wife_id = indivdualList.get(i).getID();
 						wife_name = indivdualList.get(i).getName();
+						wife_gender = indivdualList.get(i).getGender();
 						wife_death = indivdualList.get(i).getDeath();
 						wife_birth = indivdualList.get(i).getBirthday();
 					}
 				}
 				us15_fewer_than_15_siblings(fam.getChildren(), fam.getID());
 				us16_male_last_names(husband_name, wife_name, fam.getID());
+                for (String child_id : children) {
+                    us12_parents_not_too_old(fam.getID(), husband_birth, wife_birth, child_id, indivdualList);
+                }
+                us21_correct_gender_for_role(fam.getID(), husband_gender, wife_gender);
 			}
 			return errors;
 		}
@@ -186,7 +196,139 @@ public class Sprint2_Checkout {
     }
 	// US07 and US08 are done by Xudong
 
-	// US12 and US21 are done by Chenglin
+    // US07: Death should be less than 150 years after birth for dead people,
+    // and current date should be less than 150 years after birth for all living
+    // people
+    public static boolean us07_less_than_150_years_old(Date birth, Date death, String id) {
+        if (birth != null) {
+            int birth_year = Integer
+                    .parseInt(new SimpleDateFormat("yyyy-MM-dd").format(birth)
+                            .substring(0, 4));
+            if (death != null) {
+                int death_year = Integer.parseInt(new SimpleDateFormat(
+                        "yyyy-MM-dd").format(death).substring(0, 4));
+                if ((death_year - birth_year) >= 150) {
+                    String error = "ERROR: INDIVIDUAL: US07: " + id
+                            + ": Age is more than 150.";
+                    addError(errors, "US07", error);
+                    return false;
+                }
+            } else {
+                int current_year = Integer.parseInt(new SimpleDateFormat(
+                        "yyyy-MM-dd").format(new Date()).substring(0, 4));
+                if ((current_year - birth_year) >= 150) {
+                    String error = "ERROR: INDIVIDUAL: US07: " + id
+                            + ": Age is more than 150.";
+                    addError(errors, "US07", error);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
+    // US08: Children should be born after marriage of parents (and not more
+    // than 9 months after their divorce)
+    public static boolean us08_birth_before_marriage_of_parents(Date birth, Date marriage, Date divorce, String id) {
+        if (birth != null) {
+            if (marriage != null && birth.before(marriage)) {
+                String child_birth = new SimpleDateFormat("yyyy-MM-dd")
+                        .format(birth);
+                String parents_marriage = new SimpleDateFormat("yyyy-MM-dd")
+                        .format(marriage);
+                String error = "ERROR: INDIVIDUAL: US08: " + id + ": Born on "
+                        + child_birth + " before parents marry on "
+                        + parents_marriage;
+                addError(errors, "US08", error);
+                return false;
+            }
+            if (divorce != null) {
+                if (birth.getTime() - divorce.getTime() >= (1000 * 3600 * 24 * 30 * 9)) {
+                    String child_birth = new SimpleDateFormat("yyyy-MM-dd")
+                            .format(birth);
+                    String parents_divorce = new SimpleDateFormat("yyyy-MM-dd")
+                            .format(divorce);
+                    String error = "ERROR: INDIVIDUAL: US08: "
+                            + id
+                            + ": Born on "
+                            + child_birth
+                            + " after more than 9 months after parents divorced on "
+                            + parents_divorce;
+                    addError(errors, "US08", error);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+	// US12 and US21 are done by Chenglin
+    public static boolean us12_parents_not_too_old(String family_id, Date husband_birth, Date wife_birth, String child_id, List<Indivdual> indivdualList) {
+        boolean result = true;
+        for (int j = 0; j < indivdualList.size(); j++) {
+            if (child_id.equals(indivdualList.get(j).getID())) {
+                Date child_birth = indivdualList.get(j).getBirthday();
+                boolean resFather = us12_helper_check_age_difference(husband_birth, child_birth);
+                boolean resMother = us12_helper_check_age_difference(wife_birth, child_birth);
+                if (resFather && resMother) {
+                    result = true;
+                }
+                if (resFather == false) {
+                    result = false;
+                    addError(errors, "US12", "ERROR: FAMILY: US12: " + family_id
+                            + ": Father is more than 60 years older than child (" + child_id + ").");
+                }
+                if (resMother == false){
+                    result = false;
+                    addError(errors, "US12", "ERROR: FAMILY: US12: " + family_id
+                            + ": Mother is more than 60 years older than child (" + child_id + ").");
+                }
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean us12_helper_check_age_difference(Date parentBirthday, Date childBirthday) {
+        int parentYear = Integer.parseInt(new SimpleDateFormat("yyyy-MM-dd").format(parentBirthday).substring(0, 4));
+        int parentMonth = Integer.parseInt(new SimpleDateFormat("yyyy-MM-dd").format(parentBirthday).substring(5, 7));
+        int parentDay = Integer.parseInt(new SimpleDateFormat("yyyy-MM-dd").format(parentBirthday).substring(8, 10));
+
+        int childYear = Integer.parseInt(new SimpleDateFormat("yyyy-MM-dd").format(childBirthday).substring(0, 4));
+        int childMonth = Integer.parseInt(new SimpleDateFormat("yyyy-MM-dd").format(childBirthday).substring(5, 7));
+        int childDay = Integer.parseInt(new SimpleDateFormat("yyyy-MM-dd").format(childBirthday).substring(8, 10));
+
+        int yearsDiff = childYear - parentYear;
+        int monthsDiff = childMonth - parentMonth;
+        int daysInDiff = childDay - parentDay;
+
+        boolean result;
+        if (yearsDiff < 60) result = true;
+        else if (yearsDiff == 60) {
+            if (monthsDiff < 0) {
+                result = true;
+            } else if (monthsDiff == 0) {
+                if (daysInDiff <= 0) {
+                    result = true;
+                } else result = false;
+            } else result = false;
+        } else result = false;
+        return result;
+    }
+
+    public static boolean us21_correct_gender_for_role(String family_id, String husband_gender, String wife_gender) {
+        boolean result = true;
+        if (!husband_gender.equals("M")) {
+            addError(errors, "US21", "ERROR: FAMILY: US21: " + family_id
+                    + ": Wrong gender for husband.");
+            result = false;
+        }
+        if (!wife_gender.equals("F")) {
+            addError(errors, "US21", "ERROR: FAMILY: US21: " + family_id
+                    + ": Wrong gender for wife.");
+            result = false;
+        }
+        return result;
+    }
 
 	}
