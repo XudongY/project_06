be6edfa09e6d003b39065a957974f906b4e1bd6a
dsareen02 <32178689+dsareen02@users.noreply.github.com>
2017-11-08 14:23:24 -0500

    Add files via upload

---
+++ Sprint3_Checkout.java
@@ -0,0 +1,236 @@
+package sprints;
+
+import important.Family;
+import important.Indivdual;
+import jnr.ffi.annotations.In;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.*;
+
+public class Sprint3_Checkout {
+    public static HashMap<String, Date> upcoming_birthdays=new HashMap<>();
+    public static HashMap<String, Date> recent_births = new HashMap<String, Date>();
+    public static HashMap<String, Date> recent_deaths = new HashMap<String, Date>();
+    public static HashMap<String, List<String>> errors = new HashMap<String, List<String>>();
+
+    public static HashMap<String, List<String>> check_List(List<Indivdual> indivdualList, List<Family> familyList) {
+
+        for (Family fam : familyList) {
+            String husband_id = null;
+            String husband_name = null;
+            String husband_gender = null;
+            Date husband_death = null;
+            Date husband_birth = null;
+
+            String wife_id = null;
+            String wife_name = null;
+            String wife_gender = null;
+            Date wife_death = null;
+            Date wife_birth = null;
+            Date child_birth = null;
+
+            String childName;
+            LocalDate childBirth;
+            int childAge;
+
+            for (int i = 0; i < indivdualList.size(); ++i) {
+                String ID=indivdualList.get(i).getID();
+                Date Deathday=indivdualList.get(i).getDeath();
+                Date Birthday=indivdualList.get(i).getBirthday();
+                if(Deathday!=null)
+                {
+                   check_for_upcoming_birthdays(ID,Birthday);
+                }
+                if (fam.getHusbandID().equals(indivdualList.get(i).getID())) {
+                    husband_id = indivdualList.get(i).getID();
+                    husband_name = indivdualList.get(i).getName();
+                    husband_death = indivdualList.get(i).getDeath();
+                    husband_birth = indivdualList.get(i).getBirthday();
+                    husband_gender = indivdualList.get(i).getGender();
+                    us10_marriage_after_14(fam.getMarried(),husband_birth,husband_id);
+                    if(husband_death != null) {
+                        for (String child_id : fam.getChildren()) {
+                            for (Indivdual in2 : indivdualList) {
+                                if (child_id.equals(in2.getID())) {
+                                    us09_birth_before_death_of_parents(husband_death, in2.getBirthday(), in2.getID());
+                                }
+                            }
+                        }
+                    }
+                }
+                if (fam.getWifeID().equals(indivdualList.get(i).getID())) {
+                    wife_id = indivdualList.get(i).getID();
+                    wife_name = indivdualList.get(i).getName();
+                    wife_death = indivdualList.get(i).getDeath();
+                    wife_birth = indivdualList.get(i).getBirthday();
+                    wife_gender = indivdualList.get(i).getGender();
+                    us10_marriage_after_14(fam.getMarried(),wife_birth,wife_id);
+                    if(wife_death != null) {
+                        for (String child_id : fam.getChildren()) {
+                            for (Indivdual in2 : indivdualList) {
+                                if (child_id.equals(in2.getID())) {
+                                    us09_birth_before_death_of_parents(wife_death, in2.getBirthday(), in2.getID());
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (husband_death != null && wife_death != null) {
+                    for (String child_id : fam.getChildren()) {
+                        if (child_id.equals(indivdualList.get(i).getID())) {
+                            childName = indivdualList.get(i).getName();
+                            child_birth = indivdualList.get(i).getBirthday();
+                            childBirth = child_birth.toInstant()
+                                    .atZone(ZoneId.systemDefault())
+                                    .toLocalDate();
+                            childAge = us33_helper_calculateAge(childBirth);
+                            us33_list_orphans(childAge, husband_death, wife_death, childName, child_id, fam.getID());
+                        }
+                    }
+                }
+            }
+            us34_list_large_age_differences(husband_birth, wife_birth, fam.getMarried(), fam.getID());
+        }
+        return errors;
+    }
+
+
+    public static boolean us10_marriage_after_14(Date marriage, Date birth, String ID) {
+        int birth_year = Integer
+                .parseInt(new SimpleDateFormat("yyyy-MM-dd").format(birth)
+                        .substring(0, 4));
+        if (marriage != null) {
+            int death_year = Integer.parseInt(new SimpleDateFormat(
+                    "yyyy-MM-dd").format(marriage).substring(0, 4));
+            if ((death_year - birth_year) <= 14) {
+                String error = "ERROR: INDIVIDUAL: US10: " + ID
+                        + ": marriage before 14.";
+                addError(errors, "US10", error);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean us09_birth_before_death_of_parents(Date parent_death, Date child_birth, String ID) {
+        if(child_birth.after(parent_death)) {
+            String error = "ERROR: INDIVIDUAL: US09: " + ID
+                    + ": Birth after death of parents.";
+            addError(errors, "US10", error);
+            return false;
+        }
+        return true;
+    }
+
+    //US33 & US34 by Chenglin Wu
+    public static boolean us33_list_orphans(int childAge, Date fatherDeath, Date motherDeath, String childName, String childID, String famID) {
+        if (childAge > 0 && childAge < 18 && fatherDeath != null && motherDeath != null) {
+            addError(errors, "US33", "ORPHANS: INDIVIDUAL: US33: " + childName + "(" + childID + ")" + " is orphan.");
+            return true;
+        } else return false;
+    }
+
+    public static int us33_helper_calculateAge(LocalDate birthDate) {
+        if ((birthDate != null)) {
+            return Period.between(birthDate, LocalDate.now()).getYears();
+        } else {
+            return 0;
+        }
+    }
+
+    public static boolean us34_list_large_age_differences(Date husband_birth, Date wife_birth, Date merriage, String famID) {
+        LocalDate husbandBirth = husband_birth.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        LocalDate wifeBirth = wife_birth.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        LocalDate merriageDate = merriage.toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+        if (us34_helper_calculateAge(husbandBirth, merriageDate) > us34_helper_calculateAge(wifeBirth, merriageDate)) {
+            if (us34_helper_calculateAge(husbandBirth, merriageDate) - us34_helper_calculateAge(wifeBirth, merriageDate)
+                    >= us34_helper_calculateAge(wifeBirth, merriageDate)) {
+                addError(errors, "US34", "Large Age Difference: Family: " + famID + " Husband is more than twice as old as wife.");
+                return true;
+            } else return false;
+        } else if (us34_helper_calculateAge(husbandBirth, merriageDate) < us34_helper_calculateAge(wifeBirth, merriageDate)) {
+            if (us34_helper_calculateAge(wifeBirth, merriageDate) - us34_helper_calculateAge(husbandBirth, merriageDate)
+                    >= us34_helper_calculateAge(husbandBirth, merriageDate)) {
+                addError(errors, "US34", "Large Age Difference: Family: " + famID + " Wife is more than twice as old as husband.");
+                return true;
+            } else return false;
+        } else return false;
+    }
+
+    public static int us34_helper_calculateAge(LocalDate birthDate, LocalDate merriage) {
+        if ((birthDate != null)) {
+            return Period.between(birthDate, merriage).getYears();
+        } else {
+            return 0;
+        }
+    }
+    //us38 and us39 are  done  by DISHA
+    public static boolean check_for_upcoming_birthdays(String ID, Date Birthday)
+    {
+        try{
+
+            SimpleDateFormat format=new SimpleDateFormat("dd MM yyyy");
+            Calendar cal=Calendar.getInstance();
+            String current_date_String_Format=format.format(cal.getTime());
+            Date current_date_Date_Format=format.parse(current_date_String_Format);
+            cal.add(Calendar.DATE, +30);
+            String forward_date_String_Format=format.format(cal.getTime());
+            Date forward_date_Date_Format=format.parse(forward_date_String_Format);
+
+            if((Birthday.before(forward_date_Date_Format))&&(Birthday.after(current_date_Date_Format))){
+                upcoming_birthdays.put(ID,Birthday);
+            }
+            else
+            {return false;}
+
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    // US38:List all living people in a GEDCOM file whose birthdays occur in the next 30 days
+    public static void us38_list_upcoming_birthdas(HashMap map) {
+
+        Set mapSet = (Set) map.entrySet();
+        Iterator mapIterator = mapSet.iterator();
+        while (mapIterator.hasNext()) {
+            Map.Entry mapEntry = (Map.Entry) mapIterator.next();
+            // getKey Method of HashMap access a key of map
+            String keyValue = (String) mapEntry.getKey();
+            // getValue method returns corresponding key's value
+            Date value = (Date) mapEntry.getValue();
+            String d = new SimpleDateFormat("dd MMM yyyy").format(value);
+            String error = "PRINT: INDIVIDUAL: US38: " + keyValue
+                    + ": Birthday s upcoming " + d + " in recent 30 days.";
+            addError(errors, "US38", error);
+        }
+    }
+
+    public static void addError(HashMap<String, List<String>> errors,
+                                String key, String value) {
+        if (errors.containsKey(key)) {
+            List<String> res = errors.get(key);
+            res.add(value);
+            errors.put(key, res);
+        } else {
+            List<String> res = new ArrayList<String>();
+            res.add(value);
+            errors.put(key, res);
+        }
+    }
+}
---
+++ TestSuites_Sprint3.java
@@ -0,0 +1,129 @@
+package test;
+
+//import com.sun.source.tree.AssertTree;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Assert;
+import sprints.Sprint2_Checkout;
+import sprints.Sprint3_Checkout;
+//import sun.jvm.hotspot.utilities.Assert;
+
+public class TestSuites_Sprint3 extends TestCase {
+    public TestSuites_Sprint3() {
+        super();
+    }
+
+    public TestSuites_Sprint3(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+
+    }
+    public void Birth_before_death_of_parents () throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+        Date date1 = simpleDateFormat.parse(new String("24 SEP 1995"));
+        Date date2 = simpleDateFormat.parse(new String("22 FEB 1994"));
+        String ID = "1111";
+        Date date3 = simpleDateFormat.parse(new String("24 SEP 1995"));
+        Date date4 = simpleDateFormat.parse(new String("22 FEB 2001"));
+        String ID2 = "1111";
+        boolean res = Sprint3_Checkout.us09_birth_before_death_of_parents(date1,date2,ID);
+        boolean res2 = Sprint3_Checkout.us09_birth_before_death_of_parents(date3,date4,ID2);
+        System.out.println(res);
+        System.out.println(res2);
+        org.junit.Assert.assertTrue("US09 is false", res == true);
+        org.junit.Assert.assertFalse("US09 is true", res2 == true);
+    }
+
+    public void marriage_after_14 () throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+        Date date1 = simpleDateFormat.parse(new String("24 SEP 1995"));
+        Date date2 = simpleDateFormat.parse(new String("22 FEB 1994"));
+        String ID = "1111";
+        Date date3 = simpleDateFormat.parse(new String("24 SEP 1995"));
+        Date date4 = simpleDateFormat.parse(new String("22 FEB 1966"));
+        String ID2 = "1111";
+        boolean res = Sprint3_Checkout.us10_marriage_after_14(date1,date2,ID);
+        boolean res2 = Sprint3_Checkout.us10_marriage_after_14(date3,date4,ID2);
+        System.out.println(res);
+        System.out.println(res2);
+        org.junit.Assert.assertFalse("US10 is false", res == true);
+        org.junit.Assert.assertTrue("US10 is true", res2 == true);
+    }
+
+    public void isOrphans () throws ParseException {
+        int childAge1 = 15;
+        int childAge2 = 18;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM yyyy");
+        Date fatherDeath1 = null;
+        Date fatherDeath2 = simpleDateFormat.parse(new String ("18 02 2011"));
+        Date motherDeath1 = null;
+        Date motherDeath2 = simpleDateFormat.parse(new String ("18 02 2011"));
+        boolean res1 = Sprint3_Checkout.us33_list_orphans(childAge1, fatherDeath1, motherDeath1, "child1", "I01", "F01"); //False
+        boolean res2 = Sprint3_Checkout.us33_list_orphans(childAge1, fatherDeath1, motherDeath2, "child1", "I01", "F02"); //False
+        boolean res3 = Sprint3_Checkout.us33_list_orphans(childAge1, fatherDeath2, motherDeath1, "child1", "I01", "F03"); //False
+        boolean res4 = Sprint3_Checkout.us33_list_orphans(childAge1, fatherDeath2, motherDeath2, "child1", "I01", "F04"); //True
+        boolean res5 = Sprint3_Checkout.us33_list_orphans(childAge2, fatherDeath1, motherDeath1, "child2", "I02", "F05"); //False
+        boolean res6 = Sprint3_Checkout.us33_list_orphans(childAge2, fatherDeath1, motherDeath2, "child2", "I02", "F06"); //False
+        boolean res7 = Sprint3_Checkout.us33_list_orphans(childAge2, fatherDeath2, motherDeath1, "child2", "I02", "F07"); //False
+        boolean res8 = Sprint3_Checkout.us33_list_orphans(childAge2, fatherDeath2, motherDeath2, "child2", "I02", "F08"); //False
+        org.junit.Assert.assertFalse("Test case 1 is failed.", res1);
+        org.junit.Assert.assertFalse("Test case 2 is failed.", res2);
+        org.junit.Assert.assertFalse("Test case 3 is failed.", res3);
+        org.junit.Assert.assertTrue("Test case 4 is failed.", res4);
+        org.junit.Assert.assertFalse("Test case 5 is failed.", res5);
+        org.junit.Assert.assertFalse("Test case 6 is failed.", res6);
+        org.junit.Assert.assertFalse("Test case 7 is failed.", res7);
+        org.junit.Assert.assertFalse("Test case 8 is failed.", res8);
+    }
+
+    public void isLargeAgeDifference () throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM yyyy");
+        Date merriage = simpleDateFormat.parse(new String ("18 09 2017"));
+        Date fatherBirth1 = simpleDateFormat.parse(new String ("18 02 1997"));
+        Date fatherBirth2 = simpleDateFormat.parse(new String ("18 02 1977"));
+        Date motherBirth1 = simpleDateFormat.parse(new String ("18 02 1997"));
+        Date motherBirth2 = simpleDateFormat.parse(new String ("18 02 1977"));
+        boolean res1 = Sprint3_Checkout.us34_list_large_age_differences(fatherBirth1, motherBirth1, merriage, "F01"); //False
+        boolean res2 = Sprint3_Checkout.us34_list_large_age_differences(fatherBirth1, motherBirth2, merriage, "F01"); //True
+        boolean res3 = Sprint3_Checkout.us34_list_large_age_differences(fatherBirth2, motherBirth1, merriage, "F01"); //True
+        org.junit.Assert.assertFalse("Test case 1 is failed.", res1);
+        org.junit.Assert.assertTrue("Test case 2 is failed.", res2);
+        org.junit.Assert.assertTrue("Test case 3 is failed.", res3);
+    }
+    public void upcoming_birthdays() {
+
+        String dateInString_1 = "10 JAN 2016";
+        String dateInString_2 = "10 NOV 2017";
+        SimpleDateFormat format = new SimpleDateFormat("dd MMM yyyy");
+        try {
+            Date date_Date_format_1 = format.parse(dateInString_1);
+            Date date_Date_format_2 = format.parse(dateInString_2);
+            boolean res1 = Sprint3_Checkout.check_for_upcoming_birthdays(
+                    "I@134253@", date_Date_format_1);
+            Assert.assertTrue("US38 is false", res1 == false);
+            boolean res3 = Sprint2_Checkout.check_for_recent_births(
+                    "I@1333353@", date_Date_format_2);
+            Assert.assertTrue("US38 is true", res3 == true);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static junit.framework.Test suite() {
+        TestSuite s = new TestSuite();
+        s.addTest(new TestSuites_Sprint3("Birth_before_death_of_parents"));
+        s.addTest(new TestSuites_Sprint3("marriage_after_14"));
+        s.addTest(new TestSuites_Sprint3("isOrphans"));
+        s.addTest(new TestSuites_Sprint3("isLargeAgeDifference"));
+        s.addTest(new TestSuites_Sprint3("upcoming_birthdays"));
+        return s;
+    }
+}
