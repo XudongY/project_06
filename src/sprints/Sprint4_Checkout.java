bb60ed508e7a3e41bd2dd5e9d9d138bc1ab4bab3
HarrisYutongZhao <yutongzhao0711@gmail.com>
2017-11-14 17:26:26 -0500

    adas

--- src/sprints/Sprint4_Checkout.java
+++ src/sprints/Sprint4_Checkout.java
@@ -26,14 +26,34 @@ public class Sprint4_Checkout {
     public static HashMap<String, List<String>> errors = new HashMap<String, List<String>>();
     public static HashSet<String> uniqueID = new HashSet<>();
     public static HashSet<String> uniqueNameAndBirth = new HashSet<>();
+    public static HashSet<String> siblings_spacing = new HashSet<>();
 
-    public static HashMap<String, List<String>> check_List(
-            List<Indivdual> indivdualList, List<Family> familyList) {
+    public static HashMap<String, List<String>> check_List(List<Indivdual> indivdualList, List<Family> familyList) {
         us22_Unique_IDs(indivdualList);
         us23_Unique_name_and_birth_date(indivdualList);
+
+        for(Indivdual indi: indivdualList){
+
+        }
+
+        for(Family fam: familyList){
+            if(fam.getChildren()!=null || fam.getChildren().size()!=0){
+                List<Date> children_birthdays = new ArrayList<>();
+                for(String child_id: fam.getChildren()){
+                    for(Indivdual indi: indivdualList){
+                        if(child_id == indi.getID()){
+                            children_birthdays.add(indi.getBirthday());
+                        }
+                    }
+                }
+
+            }
+        }
+
         return errors;
     }
 
+    // US22 and US23 are done by Xudong
     public static boolean us22_Unique_IDs(List<Indivdual> indivdualList) {
         for (Indivdual in : indivdualList) {
             if (uniqueID.contains(in.getID())) {
@@ -63,6 +83,7 @@ public class Sprint4_Checkout {
     }
 
 
+
     public static void addError(HashMap<String, List<String>> errors,
                                 String key, String value) {
         if (errors.containsKey(key)) {
@@ -75,4 +96,12 @@ public class Sprint4_Checkout {
             errors.put(key, res);
         }
     }
+
+    // US13 and US27 are done by Yutong Zhao
+    // US13: Birth dates of siblings should be more than 8 months apart
+    // or less than 2 days apart (twins may be born one day apart, e.g. 11:59 PM and 12:02 AM the following calendar day)
+
+
+    // US27:Include person's current age when listing individuals
+
 }
