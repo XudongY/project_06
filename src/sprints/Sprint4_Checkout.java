b961f9fa95e7156446b2a27154b5f73f9a5160ef
chenglinwu <jasonwu.601@gmail.com>
2017-11-22 17:09:55 -0500

    update US24&25

--- src/sprints/Sprint4_Checkout.java
+++ src/sprints/Sprint4_Checkout.java
@@ -30,24 +30,26 @@ public class Sprint4_Checkout {
     public static HashMap<String, List<String>> check_List(List<Indivdual> indivdualList, List<Family> familyList) {
         us22_Unique_IDs(indivdualList);
         us23_Unique_name_and_birth_date(indivdualList);
+        us24_unique_family_by_spouses(familyList);
         us27_include_individual_ages(indivdualList);
 
-        for(Indivdual indi: indivdualList){
+        for (Indivdual indi : indivdualList) {
 
         }
 
-        for(Family fam: familyList){
-            if(fam.getChildren()!=null && fam.getChildren().size()>1){
+        for (Family fam : familyList) {
+            if (fam.getChildren() != null && fam.getChildren().size() > 1) {
                 List<Indivdual> children = new ArrayList<>();
-                for(String child_id: fam.getChildren()){
-                    for(Indivdual indi: indivdualList){
-                        if(child_id.equals(indi.getID())){
+                for (String child_id : fam.getChildren()) {
+                    for (Indivdual indi : indivdualList) {
+                        if (child_id.equals(indi.getID())) {
                             children.add(indi);
                         }
                     }
                 }
                 us13_siblings_spacing(children);
             }
+            us25_Unique_first_names_in_families(fam.getChildren(), indivdualList, fam.getID());
         }
 
         return errors;
@@ -83,7 +85,6 @@ public class Sprint4_Checkout {
     }
 
 
-
     public static void addError(HashMap<String, List<String>> errors,
                                 String key, String value) {
         if (errors.containsKey(key)) {
@@ -100,52 +101,89 @@ public class Sprint4_Checkout {
     // US13 and US27 are done by Yutong Zhao
     // US13: Birth dates of siblings should be more than 8 months apart
     // or less than 2 days apart (twins may be born one day apart, e.g. 11:59 PM and 12:02 AM the following calendar day)
-    public static void us13_siblings_spacing(List<Indivdual> children){
-        for(int i=0; i<children.size()-1; i++){
-            System.out.println(children.get(i).getID()+"\t");
-            for(int j=0; j<i; j++){
-                if(!us13_compare_birthday(children.get(i).getBirthday(), children.get(j).getBirthday())){
-                    addError(errors,"US13","ERROR: FAMILY: US13: "+children.get(i).getID()+
-                            ": has spacing conflicts with "+children.get(j).getID());
+    public static void us13_siblings_spacing(List<Indivdual> children) {
+        for (int i = 0; i < children.size() - 1; i++) {
+            System.out.println(children.get(i).getID() + "\t");
+            for (int j = 0; j < i; j++) {
+                if (!us13_compare_birthday(children.get(i).getBirthday(), children.get(j).getBirthday())) {
+                    addError(errors, "US13", "ERROR: FAMILY: US13: " + children.get(i).getID() +
+                            ": has spacing conflicts with " + children.get(j).getID());
                 }
             }
-            for(int j=i+1; j<children.size(); j++){
-                if(!us13_compare_birthday(children.get(i).getBirthday(), children.get(j).getBirthday())){
-                    addError(errors,"US13","ERROR: FAMILY: US13: "+children.get(i).getID()+
-                            ": has spacing conflicts with "+children.get(j).getID());
+            for (int j = i + 1; j < children.size(); j++) {
+                if (!us13_compare_birthday(children.get(i).getBirthday(), children.get(j).getBirthday())) {
+                    addError(errors, "US13", "ERROR: FAMILY: US13: " + children.get(i).getID() +
+                            ": has spacing conflicts with " + children.get(j).getID());
                 }
             }
         }
     }
 
-    public static boolean us13_compare_birthday(Date b1, Date b2){
+    public static boolean us13_compare_birthday(Date b1, Date b2) {
         long birth1 = b1.getTime();
         long birth2 = b2.getTime();
         long day = Long.parseLong("86400000");
-        if(Math.abs(birth1-birth2)>2*day && Math.abs(birth1-birth2)<8*30*day){
+        if (Math.abs(birth1 - birth2) > 2 * day && Math.abs(birth1 - birth2) < 8 * 30 * day) {
             return false;
         }
         return true;
     }
 
     // US27:Include person's current age when listing individuals
-    public static void us27_include_individual_ages(List<Indivdual> indivdualList){
-        for(Indivdual indi: indivdualList){
-            if(indi.getDeath()== null){
+    public static void us27_include_individual_ages(List<Indivdual> indivdualList) {
+        for (Indivdual indi : indivdualList) {
+            if (indi.getDeath() == null) {
                 String age = age_calculator(indi.getBirthday(), new Date());
-                addError(errors,"US27","PRINT: INDIVIDUAL: "+indi.getID()+": "+indi.getName()+" is "+age +" years old now.");
-            }else{
+                addError(errors, "US27", "PRINT: INDIVIDUAL: " + indi.getID() + ": " + indi.getName() + " is " + age + " years old now.");
+            } else {
                 String age = age_calculator(indi.getBirthday(), indi.getDeath());
-                addError(errors,"US27","PRINT: INDIVIDUAL: "+indi.getID()+": "+indi.getName()+" paased at the age of "+age +".");
+                addError(errors, "US27", "PRINT: INDIVIDUAL: " + indi.getID() + ": " + indi.getName() + " paased at the age of " + age + ".");
             }
         }
     }
 
-    public static String age_calculator(Date birth, Date current){
+    public static String age_calculator(Date birth, Date current) {
         long diff = current.getTime() - birth.getTime();
-        long days = 24*60*60*1000;
-        int age = (int)Math.floor(diff/(days*365));
+        long days = 24 * 60 * 60 * 1000;
+        int age = (int) Math.floor(diff / (days * 365));
 
         return String.valueOf(age);
+    }
+
+    //US24 & US25
+    //US24: No more than one family with the same spouses by name and the same marriage date should appear in a GEDCOM file
+    public static boolean us24_unique_family_by_spouses(List<Family> families) {
+        HashMap<String, Date> husbandHash = new HashMap<>();
+        HashMap<String, Date> wifeHash = new HashMap<>();
+        boolean result = true;
+        for (Family f : families) {
+            if (husbandHash.containsKey(f.getHusbandName()) && husbandHash.get(f.getHusbandName()).equals(f.getMarried())) {
+                addError(errors, "US24", "ERROR: FAMILY: US24: " + f.getID() + ": " + f.getHusbandName() + " has more than one marriage on the same day");
+                result = false;
+            } else husbandHash.put(f.getHusbandName(), f.getMarried());
+            if (wifeHash.containsKey(f.getWifeName()) && wifeHash.get(f.getWifeName()).equals(f.getMarried())) {
+                addError(errors, "US24", "ERROR: FAMILY: US24: " + f.getID() + ": " + f.getWifeName() + " has more than one marriage on the same day");
+                result = false;
+            } else wifeHash.put(f.getWifeName(), f.getMarried());
+        }
+        return result;
+    }
+
+    //US25: No more than one child with the same name and birth date should appear in a family
+    public static boolean us25_Unique_first_names_in_families(List<String> children, List<Indivdual> indivduals, String famID) {
+        HashMap<String, Date> hashMap = new HashMap<>();
+        boolean result = true;
+        for (String child : children) {
+            for (Indivdual i : indivduals) {
+                if (child.equals(i.getID())) {
+                    String firstName = child.split("/")[0];
+                    if (hashMap.containsKey(firstName) && hashMap.get(firstName).equals(i.getBirthday())) {
+                        addError(errors, "US25", "ERROR: FAMILY: US25: " + famID + ": " + i.getName() + " has same first name and Birthday with another child in the family");
+                        result = false;
+                    } else hashMap.put(firstName, i.getBirthday());
+                }
+            }
+        }
+        return result;
     }
 }
